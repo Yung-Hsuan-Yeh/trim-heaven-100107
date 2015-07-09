06ce08eb0424962664f4e4abe5196e0a89cee543
Yung-Hsuan-Yeh <abyo777@gmail.com>
2015-07-09 16:47:12 +0800

    """

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def hello():
     return 'Hello World!'
 
 
-@app.errorhandler(404)
+"""@app.errorhandler(404)
 def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+    Return a custom 404 error.
+    return 'Sorry, nothing at this URL.', 404"""
