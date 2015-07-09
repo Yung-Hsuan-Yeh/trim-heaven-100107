4f19f20ebcf0e24b26af2b6dccd50b512a456e10
Yung-Hsuan-Yeh <abyo777@gmail.com>
2015-07-09 17:01:52 +0800

    main

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
-
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
 
 # encoding: utf-8
 
